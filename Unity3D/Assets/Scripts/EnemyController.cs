3885489ae13b7a972c29b832c43ae6490d6e0011
admin <answn4450@gmail.com>
2023-06-12 13:50:39 +0900

    카메라 움직임 등

--- Unity3D/Assets/Scripts/EnemyController.cs
+++ Unity3D/Assets/Scripts/EnemyController.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class EnemyController : MonoBehaviour
 {
+    [Range(0.0f, 180.0f)]
+    public float Angle;
+
     public Node Target;
 
     private float Speed;
 
+    private int check;
+    private bool move;
+
+    private Camera camera;
+
     private Vector3 LeftCheck, RightCheck;
+
+    private Vector3 offset;
+
+    private bool View;
 
     private void Awake()
     {
+        camera = Camera.main;
+
         SphereCollider coll = GetComponent<SphereCollider>();
         coll.radius = 0.05f;
         coll.isTrigger = true;
@@ -21,11 +35,16 @@ public class EnemyController : MonoBehaviour
         Rigidbody rigid = GetComponent<Rigidbody>();
         rigid.useGravity = false;
 
-        Target = GameObject.Find("ParentObject").transform.GetChild(0).GetComponent<Node>();
+        offset = new Vector3(0.0f, 10.0f, 0.0f);
+        //Target = GameObject.Find("ParentObject").transform.GetChild(0).GetComponent<Node>();
+        View = false;
     }
 
     private void Start()
     {
+        Angle = 45.0f;
+        move = false;
+
         Speed = 3.0f;
 
         float x = 2.5f;
@@ -37,32 +56,112 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
+        View = (Input.GetKey(KeyCode.Tab)) ? true:false;
+
+        if (View)
+        {
+            offset = new Vector3(0.0f, 5.0f, -3.0f);
+            camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, 100.0f, Time.deltaTime);
+        }
+        else
+        {
+            offset = new Vector3(0.0f, 10.0f, -10.0f);
+            camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, 60.0f, Time.deltaTime);
+        }
+
+        camera.transform.position = Vector3.Lerp(
+            camera.transform.position,
+            transform.position + offset,
+            0.016f
+            );
+
+        camera.transform.LookAt(transform.position);
+
         if (Target)
         {
-            Vector3 Direction = (Target.transform.position - transform.position).normalized;
-            //transform.Translate(Direction * Time.deltaTime * Speed);
-            transform.position += Direction * Time.deltaTime * Speed;
-
-            Ray ray = new Ray(transform.position, Direction);
-
-            RaycastHit hit;
-
-            //transform.LookAt(Target.transform);
-
-            //Debug.DrawRay(transform.position, Direction * 5.0f, Color.red);
-            Debug.DrawRay(transform.position, RightCheck, Color.red);
-            Debug.DrawRay(transform.position, LeftCheck, Color.red);
-
-            if (Physics.Raycast(transform.position, LeftCheck, out hit, 5.0f))
+            if (move)
             {
-
+                Vector3 Direction = (Target.transform.position - transform.position).normalized;
+                transform.position += Direction * Time.deltaTime * Speed;
             }
-
-            if (Physics.Raycast(transform.position, RightCheck, out hit, 5.0f))
+            else
             {
-
+                transform.rotation = Quaternion.Lerp(
+                    transform.rotation,
+                     Quaternion.LookRotation(transform.position, Target.transform.position),
+                    Time.deltaTime);
             }
         }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, RightCheck, Color.red);
+        Debug.DrawRay(transform.position, LeftCheck, Color.red);
+
+        if (Physics.Raycast(transform.position, LeftCheck, out hit, 5.0f))
+        {
+
+        }
+
+        if (Physics.Raycast(transform.position, RightCheck, out hit, 5.0f))
+        {
+
+        }
+
+        /*
+        Debug.DrawRay(transform.position,
+            new Vector3(
+                Mathf.Sin(Angle * Mathf.Deg2Rad), 0.0f, Mathf.Cos(Angle * Mathf.Deg2Rad)) * 2.5f,
+            Color.green
+            );
+
+        Debug.DrawRay(transform.position,
+            new Vector3(
+                Mathf.Sin(-Angle * Mathf.Deg2Rad), 0.0f, Mathf.Cos(Angle * Mathf.Deg2Rad)) * 2.5f,
+            Color.green
+            );
+        */
+
+        for (float f = -Angle; f < Angle; f += 5.0f)
+        {
+            Debug.DrawLine(
+                transform.position,
+                transform.position +
+                new Vector3(
+                    Mathf.Sin((transform.rotation.eulerAngles.y + f) * Mathf.Deg2Rad),
+                    0.0f,
+                    Mathf.Cos((transform.rotation.eulerAngles.y + f) * Mathf.Deg2Rad)) * 2.5f,
+                Color.white
+            );
+        }
+    }
+
+    void function()
+    {
+        if (move)
+            return;
+
+        move = true;
+        StartCoroutine(SetMove());
+    }
+
+    IEnumerator SetMove()
+    {
+        float time = 0.0f;
+
+        check = (check == 0) ? 1 : 0;
+
+        while (time < 1.0f)
+        {
+            time += Time.deltaTime;
+
+            yield return null;
+
+        }
+        move = false;
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ Unity3D/Assets/Scripts/Rotation/Rotation.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    [Range(0.0f, 1.0f)]
+    public float dis;
+
+    private Vector3 temp;
+    private Vector3 dest;
+
+    private int check;
+    private bool move;
+
+    void Start()
+    {
+        dis = 1.0f;
+
+        temp = new Vector3(0.0f, 0.0f, 0.0f);
+        dest = new Vector3(100.0f, 0.0f, 0.0f);
+        
+        check = 0;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0.0f, Input.GetAxis("Horizontal") * Time.deltaTime *50.0f, 0.0f, Space.Self);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            function();
+        }
+       
+    }
+
+    void function()
+    {
+        if (move)
+            return;
+        
+        move = true;
+        StartCoroutine(SetMove());
+    }
+
+    IEnumerator SetMove()
+    {
+        float time = 0.0f;
+
+        check = (check == 0) ? 1 : 0;
+
+        while(time < 1.0f)
+        {
+            if (check == 0)
+            {
+                transform.position = Vector3.Lerp(
+                    temp,
+                    dest,
+                    time
+                    );
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(
+                    dest,
+                    temp,
+                    time
+                    );
+            }
+
+            time += Time.deltaTime;
+
+            yield return null;
+            
+        }
+        move = false;
+    }
+}
